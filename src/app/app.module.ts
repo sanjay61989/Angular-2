b70b9cdd1117ad6042621a38f957a440f5f9db0b
Chhabria <sanjay.chhabria@capgemini.com>
2019-05-13 12:28:28 +0530

    lifecycle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms'
+import {ReactiveFormsModule,FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameEditorComponent,
     ProfileEditorComponent,
     LifecycleComponent,
   ],
   imports: [
-    BrowserModule,ReactiveFormsModule
+    BrowserModule,ReactiveFormsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrls: ['./lifecycle.component.css']
 })
-export class LifecycleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LifecycleComponent implements OnInit,OnInit,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
+  
+  name:any;
+  changeLog: any;
+  constructor() {
+    console.log("constructor called");    
+   }
+  ngOnChanges(changes:SimpleChanges) { 
+    for (let propName in changes) {
+      let chng = changes[propName];
+      let cur  = JSON.stringify(chng.currentValue);
+      let prev = JSON.stringify(chng.previousValue);
+      this.changeLog.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
+    console.log(this.changeLog);    
   }
-
+  ngOnInit() { 
+    console.log("on init called");
+  }
+  ngDoCheck() { 
+    console.log("do check called");
+  }
+  ngAfterContentInit() { 
+    console.log("after content init called");
+  }
+  ngAfterContentChecked() { 
+    console.log("after content checked called");
+  }
+  ngAfterViewInit() { 
+    console.log("after view init called");
+  }
+  ngAfterViewChecked() { 
+    console.log("after view checked called");
+  }
+  ngOnDestroy() { 
+    console.log("on destroy called");
+  }
 }
